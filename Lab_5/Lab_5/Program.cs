4c389d0b324ff3ba41a5ec6e71db0d1fed07b68a
Ross Hinman <hinman.ross@gmail.com>
2018-07-19 18:29:41 -0400

    Initial commit of Lab 5

---
+++ Lab_5/Lab_5/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string again = "t";
+            Console.WriteLine("Welcome to the Factorial Calculator!\n");
+
+            do
+            {
+                Console.WriteLine("Enter an integer that greater than 0 but less than 10:");
+                int x = Convert.ToInt16(Console.ReadLine());
+                int factor = 1;
+
+                for (int i = 1; i <= x; ++i)
+                {
+                    factor = factor * i;
+                }
+                Console.WriteLine($"{x}! = {factor}\n");
+                Console.WriteLine("Would you like to continue? (y/n)");
+
+                string repeat = Console.ReadLine();
+                repeat = repeat.ToLower();
+
+                if (repeat == "y")
+                {
+                    again = "t";
+                }
+                else if (repeat == "n")
+                {
+                    Console.WriteLine("Thank you for calculating!");
+                    again = "f";
+                }
+            } while (again == "t");
+        }
+    }
+}
